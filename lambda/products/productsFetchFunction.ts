644a559df3eab5b438d5cdbdfe78d379ebbd7964
Raphael Costa <rwilliam.sc@gmail.com>
2023-01-28 14:22:52 -0300

    feat: create the first lambda

---
+++ lambda/products/productsFetchFunction.ts
@@ -0,0 +1,41 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+
+export async function hendler(
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
+  // Id created by lambda
+  const lambdaRequestId = context.awsRequestId;
+
+  // Id created by APIgateway when invoking lambda
+  const apiRequestId = event.requestContext.resourceId;
+
+  console.log(
+    `API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`
+  );
+
+  const method = event.httpMethod;
+
+  if (event.resource === "/products") {
+    if (method === "GET") {
+      console.log("The GET method was called");
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "The GET method was called",
+        }),
+      };
+    }
+  }
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: "Bad request",
+    }),
+  };
+}
