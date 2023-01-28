60fd7fcdbdb3433dd512264f3deddeed001c69e5
Raphael Costa <rwilliam.sc@gmail.com>
2023-01-28 04:24:07 -0300

    Initial commit

---
+++ test/ecommerce_arq_aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcommerceArqAws from '../lib/ecommerce_arq_aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecommerce_arq_aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcommerceArqAws.EcommerceArqAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
